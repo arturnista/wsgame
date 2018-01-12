4d80f5e478daaaaf281702c8e6cefe5d98f99452
Artur <arturnista@gmail.com>
2018-01-12 18:10:43 -0200

    Server added

--- server.js
+++ server.js
@@ -9,28 +9,23 @@ server.engine('html', require('ejs').renderFile)
 const WebSocket = require('./src/WebSocket')
 const packageJson = require('./package.json')
 
+
 server.get('/', function(req, res, next) {
-    const now = moment()
-    res.status(200).json({
-        name: 'Gameserver',
-        version: packageJson.version,
-        status: 200,
-        now: now.toISOString(),
-        unix_now: now.unix()
-    })
-})
-
-server.get('/rooms', function(req, res, next) {
-    res.status(200).json( WebSocket.getRooms() )
-})
-
-server.get('/site', function(req, res, next) {
     res.render('site/build/index.html')
+})
+
+server.get('/gamedata/:filename', function(req, res, next) {
+    const filename = `${__dirname}/site/game_data/${req.params.filename}`
+    res.sendFile(filename)
 })
 
 server.get('/static/:filetype/:filename', function(req, res, next) {
     const filename = `${__dirname}/site/build/static/${req.params.filetype}/${req.params.filename}`
     res.sendFile(filename)
+})
+
+server.get('/rooms', function(req, res, next) {
+    res.status(200).json( WebSocket.getRooms() )
 })
 
 const port = process.env.PORT || 5002

d9bd18b40dbce77f358ecee231a83e5e687ea3bf
Artur <arturnista@gmail.com>
2018-01-19 22:05:02 -0200

    Added description to each spell

--- src/Room/GameObjects/spells.js
+++ src/Room/GameObjects/spells.js
@@ -5,22 +5,25 @@ module.exports = {
     },
     fireball: {
         name: 'Fireball',
+        description: 'Throws a fireball that pushes your enemy.',
         type: 'offensive',
         cooldown: 1000,
         knockbackMultiplier: 1,
         knockbackIncrement: 1.2,
-        moveSpeed: 400
+        moveSpeed: 400,
     },
     follower: {
         name: 'Follower',
+        description: 'Summons 3 followers, each one follows the closest player for 10 seconds.',
         type: 'offensive',
         cooldown: 10000,
         knockbackMultiplier: 1.3,
         knockbackIncrement: 1.5,
-        moveSpeed: 500
+        moveSpeed: 500,
     },
     explosion: {
         name: 'Explosion',
+        description: 'Cast an explosion of 120 radius. After 1s (casting time), all players in area receives knockback.',
         type: 'offensive',
         cooldown: 3000,
         radius: 120,
@@ -34,6 +37,7 @@ module.exports = {
     },
     reflect_shield: {
         name: 'Reflect Shield',
+        description: 'Protect the player for knockback and reflect some spells.',
         type: 'defensive',
         cooldown: 6000,
         duration: 1000,
@@ -45,6 +49,7 @@ module.exports = {
     },
     blink: {
         name: 'Blink',
+        description: 'Instant teleports the player for 300 units. After the blink, the player gets stunned for 1s.',
         type: 'defensive',
         cooldown: 5000,
         distance: 300,

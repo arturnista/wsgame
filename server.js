c888da96af42bdfe1e18b756f899782a6670720c
Artur <arturnista@gmail.com>
2018-01-28 10:32:24 -0200

    Folder restruct

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const moment = require('moment')
+const updates = require('./game_data/updates')
 const express = require('express')
 const server = express()
 const http = require('http').Server(server)
 
 server.set('views', __dirname + '')
 server.engine('html', require('ejs').renderFile)
+server.use('/gamedata', express.static(__dirname + '/game_data'))
 
 const WebSocket = require('./src/WebSocket')
 const packageJson = require('./package.json')
@@ -14,14 +16,13 @@ server.get('/', function(req, res, next) {
     res.render('site/build/index.html')
 })
 
-server.get('/gamedata/:filename', function(req, res, next) {
-    const filename = `${__dirname}/site/game_data/${req.params.filename}`
-    res.sendFile(filename)
-})
-
 server.get('/static/:filetype/:filename', function(req, res, next) {
     const filename = `${__dirname}/site/build/static/${req.params.filetype}/${req.params.filename}`
     res.sendFile(filename)
+})
+
+server.get('/rooms/updates', function(req, res, next) {
+    res.status(200).json( updates )
 })
 
 server.get('/rooms', function(req, res, next) {

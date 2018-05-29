3aa4e021ee6ade4460b3dee0927a2b9240c812aa
Artur <arturnista@gmail.com>
2018-05-29 13:06:51 -0300

    build

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const moment = require('moment')
 const express = require('express')
+const cors = require('cors')
 const server = express()
 const http = require('http').Server(server)
+
+var whitelist = ['http://localhost:3000']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  }else{
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+server.use(cors(corsOptionsDelegate))
 
 server.set('views', __dirname + '')
 server.engine('html', require('ejs').renderFile)

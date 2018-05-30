c6d89992d1db1cabd81323b83852747b14faf09e
Artur <arturnista@gmail.com>
2018-05-30 14:47:54 -0300

    AEHO

--- server.js
+++ server.js
@@ -6,13 +6,8 @@ const http = require('http').Server(server)
 
 var whitelist = ['http://localhost:3000']
 var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  }else{
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
+    const corsOptions = { origin: whitelist.indexOf(req.header('Origin')) !== -1 }
+    callback(null, corsOptions)
 }
 server.use(cors(corsOptionsDelegate))
 

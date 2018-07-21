d2021a79e458967beb27efd3c92e02abd0574c39
Artur <arturnista@gmail.com>
2018-07-21 20:16:52 -0300

    Build

--- src/Room/Room.js
+++ src/Room/Room.js
@@ -210,7 +210,7 @@ Room.prototype.userOwner = function (user) {
 Room.prototype.startGame = function (data) {
     const usersReady = this.users.every(x => x.status === 'ready')
     if(usersReady) {
-        this.gameObjectController.start(this.users, { emit: this.emit.bind(this), mapController: this.mapController, botCount: 0 })
+        this.gameObjectController.start(this.users, { emit: this.emit.bind(this), mapController: this.mapController, botCount: data.botCount || 0 })
         this.mapController.start(data && data.map)
 
         this.emit('game_will_start', { time: DELAY_TO_START, map: this.mapController.info() })

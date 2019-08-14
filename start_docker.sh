fb17a697e3d02308730994f499e9a581264a7db8
Artur Morelle Nista <arturnista@gmail.com>
2019-08-14 20:01:01 -0300

    start docker

--- start_docker.sh
+++ start_docker.sh
@@ -1,2 +1,2 @@
-docker build . -t nwgame_server
-docker run --name="nwgame" -p 5000-5020:5000-5020 -e PROTOCOL='HTTP' -e MAX_ROOMS=20 -d nwgame_server 
+docker build . -t magearena_server
+docker run --name="magearena" -p 5000-5020:5000-5020 -e PROTOCOL='HTTP' -e MAX_ROOMS=20 -d magearena_server 
